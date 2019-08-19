0027a99e409021b1cf3dbc72cf2f003693f35a97
Marius <redarked@gmail.com>
2019-08-19 22:40:32 +0200

    componentizzata

--- src/components/dashboard/dashboard.component.jsx
+++ src/components/dashboard/dashboard.component.jsx
@@ -1,31 +1,10 @@
 import React from "react";
-import MediaQuery from "react-responsive";
+import { ResponsiveComponent } from "../responsive/responsive.component";
 
 export const Dashboard = () => {
   return (
     <div style={{ margin: "300px 0px" }}>
-      <div>Device Test!</div>
-      <MediaQuery query="(min-device-width: 1224px)">
-        <div>You are a desktop or laptop</div>
-        <MediaQuery query="(min-device-width: 1824px)">
-          <div>You also have a huge screen</div>
-        </MediaQuery>
-        <MediaQuery query="(max-width: 1224px)">
-          <div>You are sized like a tablet or mobile phone though</div>
-        </MediaQuery>
-      </MediaQuery>
-      <MediaQuery query="(max-device-width: 1224px)">
-        <div>You are a tablet or mobile phone</div>
-      </MediaQuery>
-      <MediaQuery query="(orientation: portrait)">
-        <div>You are portrait</div>
-      </MediaQuery>
-      <MediaQuery query="(orientation: landscape)">
-        <div>You are landscape</div>
-      </MediaQuery>
-      <MediaQuery query="(min-resolution: 2dppx)">
-        <div>You are retina</div>
-      </MediaQuery>
+      <ResponsiveComponent />
     </div>
   );
 };
