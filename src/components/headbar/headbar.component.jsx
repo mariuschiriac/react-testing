f3a91f19b38e085620ec1776583b2f119ef2dab6
Marius <redarked@gmail.com>
2019-08-25 17:04:50 +0200

    headbar e css

---
+++ src/components/headbar/headbar.component.jsx
@@ -0,0 +1,14 @@
+import "./headbar.css";
+import React from "react";
+
+export const Headbar = () => {
+  return (
+    <>
+      <div>
+        <span className="center-top ">1/10</span>
+        <span className="right-top">2322 Eur</span>
+        <span className="left-top">torna alla domanda prima</span>
+      </div>
+    </>
+  );
+};
