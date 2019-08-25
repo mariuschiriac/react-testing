55b0202041e882214617cf8c26c50240e872aa17
Marius <redarked@gmail.com>
2019-08-25 15:51:43 +0200

    creato card component e aggiunto nel app

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Component } from "react";
 import "./App.css";
 import { Bottonestart } from "./components/bottone/bottone-start.component";
-import { Configuratore } from "./components/configuratore/configuratore.component";
+import { Card } from "./components/card/card.component";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <Bottonestart className="Component" />
+          <Card className="Component" />
         </header>
       </div>
       /*
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,5 +2,57 @@ import "./card.css";
 import React from "react";
 
 export const Card = () => {
-  return <></>;
+  return (
+    <>
+      <section className="section initial">
+        <div className="answer-group row-of-3">
+          <div className="col">
+            <div
+              className="answer js--answer"
+              data-answer-id="1"
+              data-answer-weight="60"
+            >
+              <img
+                className="answer-image js--answer-image"
+                src="answer-1-1.png"
+                alt="Ottima qualità"
+                title="Ottima qualità"
+              ></img>
+              <span className="answer-text">Ottima qualità</span>
+            </div>
+          </div>
+          <div className="col">
+            <div
+              className="answer js--answer"
+              data-answer-id="2"
+              data-answer-weight="40"
+            >
+              <img
+                className="answer-image js--answer-image"
+                src="answer-1-1.png"
+                alt="Buon rapporto qualità/prezzo"
+              ></img>
+              <span className="answer-text">Buon rapporto qualità/prezzo</span>
+            </div>
+          </div>
+          <div className="col">
+            <div
+              className="answer js--answer"
+              data-answer-id="3"
+              data-answer-weight="20"
+            >
+              <img
+                className="answer-image js--answer-image"
+                src="answer-1-1.png"
+                alt="Non mi interessa molto la qualità"
+              ></img>
+              <span className="answer-text">
+                Non mi interessa molto la qualità
+              </span>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
